fca95d121d40ea98c1108c4d85800172461e4ccc
Mahdi <mahdisadeghi.max@gmail.com>
2024-01-01 23:58:56 +0330

    Create base structure of project

--- miniproject.cpp
+++ miniproject.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+
 using namespace std;
 
-int main(){
-    
+void runGame();
+void receiveInput();
+void createGame();
+void updateGame();
+void showGame();
+void gameOver();
+
+int main()
+{
+    runGame();
+    return 0;
 }
+
+void runGame() {}
+
+void receiveInput() {}
+
+void createGame() {}
+
+void updateGame() {}
+
+void showGame() {}
+
+void gameOver() {}
